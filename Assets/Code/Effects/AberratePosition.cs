ed7258bf49f9a4c0799b8ea1a1148e2e266fbd63
Juancho <juan.rod707@gmail.com>
2018-02-24 21:54:28 -0300

    Agregado el shake del hud

---
+++ Assets/Code/Effects/AberratePosition.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Collections;
+
+public class AberratePosition : MonoBehaviour
+{
+    public float EffectDuration;
+    public float EffectMultiplier;
+    public float EffectFrequency;
+
+    public Vector3 zeroPos;
+    private float effectElapsed;
+    private float cycleElapsed;
+    private bool isOn;
+
+    void Start()
+    {
+        zeroPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (effectElapsed > 0)
+        {
+            if (cycleElapsed > 0)
+            {
+                Aberrate();
+                cycleElapsed -= Time.deltaTime;
+            }
+            else
+            {
+                cycleElapsed = EffectFrequency;
+            }
+
+            effectElapsed -= Time.deltaTime;
+            if (effectElapsed <= 0)
+            {
+                transform.localPosition = zeroPos;
+            }
+        }
+    }
+
+    public void StartEffect()
+    {
+        effectElapsed = EffectDuration;
+        cycleElapsed = EffectFrequency;
+        isOn = true;
+    }
+
+    void Aberrate()
+    {
+        var aberrateVector = Random.insideUnitSphere * EffectMultiplier;
+        aberrateVector.z = 0;
+
+        transform.localPosition = zeroPos + aberrateVector;
+    }
+}
